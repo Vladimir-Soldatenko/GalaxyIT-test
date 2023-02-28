f69959f791b2f9c3628ed8de8e75f482d269d14e
vladimir <soldatenko-vovan2@ukr.net>
2023-02-28 12:18:07 +0200

    complete section collections, promotions (without slider)

---
+++ src/js/scripts.js
@@ -0,0 +1,5 @@
+// Custom Scripts
+
+@@include('./libs/swiper(lib).js')
+@@include('swiper-collection.js')
+@@include('main.js')
---
+++ src/js/swiper-collection.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".men__collections__slider", {
+  // Optional parameters
+  direction: "vertical",
+  // freeMode: true,
+  slidesPerView: 1,
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+  // Navigation arrows
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+});
